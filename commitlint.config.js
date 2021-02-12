3f1eae638c1f0c39c16504fe6ffe9c527be22d7d
Gideon Elazar <elazar.gideon@gmail.com>
2021-02-13 00:07:24 +0200

    build: update commit types

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,21 +1,24 @@
 module.exports = {
     extends: ['@commitlint/config-conventional'],
     rules: {
-        'header-max-length': [2, 'always', 120],
+        'header-max-length': [2, 'always', 72],
         'subject-case': [0],
         'type-enum': [
             2,
             'always',
             [
+                'build',
+                'ci',
+                'chore',
+                'docs',
                 'feat',
                 'fix',
-                'docs',
-                'refactor',
-                'test',
                 'perf',
-                'bump',
-                'chore',
-                'revert'
+                'refactor',
+                'revert',
+                'style',
+                'test',
+                'bump'
             ]
         ],
         'scope-empty': [0]
